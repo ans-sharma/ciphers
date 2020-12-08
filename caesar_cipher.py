666c553b8f3217a8596af2adfb5e9077d7391888
Anshuman Kumar Sharma <35607867+ans-sharma@users.noreply.github.com>
2020-12-08 21:34:48 +0530

    first commit

---
+++ caesar_cipher.py
@@ -0,0 +1,46 @@
+def encrypt_caesar_cipher(text, increment=3):
+    letters = []
+    keys = []
+    cipher = ''
+    letter = 'a'
+    for i in range(0, 26):
+        letters.append(letter)
+        letter = chr(ord(letter) + 1)
+    for i in range(increment, 26):
+        keys.append(letters[i])
+    for i in range(0, increment):
+        keys.append(letters[i])
+    keys = [x.upper() for x in keys]
+    for l in text:
+        for i in range(0, 26):
+            if l == letters[i]:
+                cipher += keys[i]
+        if l == ' ':
+            cipher += ' '
+    return cipher
+
+
+def decrypt_caesar_cipher(text, decrement=3):
+    letters = []
+    keys = []
+    cipher = ''
+    letter = 'a'
+    for i in range(0, 26):
+        letters.append(letter)
+        letter = chr(ord(letter) + 1)
+    for i in range(decrement, 26):
+        keys.append(letters[i])
+    for i in range(0, decrement):
+        keys.append(letters[i])
+    keys = [x.upper() for x in keys]
+    for l in text:
+        for i in range(0, 26):
+            if l == keys[i]:
+                cipher += letters[i]
+        if l == ' ':
+            cipher += ' '
+    return cipher
+
+
+
+
---
+++ vigenère_cipher.py
@@ -0,0 +1,42 @@
+def encrypt_vigenere_cipher(string, key):
+    cipher_text = []
+    for i in range(len(string)):
+        x = (ord(string[i]) +
+             ord(key[i])) % 26
+        x += ord('A')
+        cipher_text.append(chr(x))
+    return "".join(cipher_text)
+
+
+def decrypt_vigenere_cipher(cipher_text, key):
+    orig_text = []
+    for i in range(len(cipher_text)):
+        x = (ord(cipher_text[i]) -
+             ord(key[i]) + 26) % 26
+        x += ord('A')
+        orig_text.append(chr(x))
+    return "".join(orig_text)
+
+
+def generateKey(string, key):
+    key = list(key)
+    if len(string) == len(key):
+        return key
+    else:
+        for i in range(len(string) -
+                       len(key)):
+            key.append(key[i % len(key)])
+    return "".join(key)
+
+
+if __name__ == "__main__":
+    string = "ANSHUMAN"
+    keyword = "PROGRAMMER"
+    key = generateKey(string, keyword)
+    cipher_text = encrypt_vigenere_cipher(string, key)
+    print("Ciphertext :", cipher_text)
+    print("Original/Decrypted Text :",
+          decrypt_vigenere_cipher(cipher_text, key))
+
+
+
